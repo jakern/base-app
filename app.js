cc933d248a3b46807f25e795bb93e7e33ed07d7f
Jacob A Kern <jacob.kern@infinitetactics.com>
2021-10-03 18:07:24 -0400

    save

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const userManager = require('express-user-manager');
-const expressApp = express();
+const { appendFile } = require('fs');
+const app = express();
 const http = require("http");
 /**
  * Setup the datastore using any of the currently supported database adapters:
@@ -11,12 +12,19 @@ const http = require("http");
  */
 const dbAdapter = 'sequelize'; // OR 'mongoose'
 const store = userManager.getDbAdapter(dbAdapter);
+const midd = userManager.get("middlewares")
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get("/restricted", [midd.loadUser, midd.loggindIn, midd.midd.authorized, (req, res, next)=>{res.send("granted")}])
 
 // Bind the routes under [apiMountPoint] (default: ***/api/users***):
-userManager.listen(expressApp, apiMountPoint = '/api/users', customRoutes = {});
+userManager.listen(app, apiMountPoint = '/api/users', customRoutes = {});
 
 (async function() {
-  const server = http.createServer(expressApp);
+  const server = http.createServer(app);
 
   // Establish a connection to the data store
   // Ensure the db is connected before binding the server to the port
@@ -25,9 +33,9 @@ userManager.listen(expressApp, apiMountPoint = '/api/users', customRoutes = {});
     // port: DB_PORT, // optional
     // user: DB_USERNAME, // optional
     // pass: DB_PASSWORD, // optional
-    // engine: DB_ENGINE, // optional if the adapter is "mongoose" or if the value is "memory" and the adapter is "sequelize"; required otherwise
+    engine: 'sqlite', // optional if the adapter is "mongoose" or if the value is "memory" and the adapter is "sequelize"; required otherwise
     // dbName: DB_DBNAME, // optional, default: 'users'
-    // storagePath: DB_STORAGE_PATH, // optional, required if "engine" is set to "sqlite"
+    storagePath: "users.db", // optional, required if "engine" is set to "sqlite"
     // debug: DB_DEBUG, // optional, default: false
     // exitOnFail: EXIT_ON_DB_CONNECT_FAIL // optional, default: true
   });
@@ -40,6 +48,6 @@ userManager.listen(expressApp, apiMountPoint = '/api/users', customRoutes = {});
 
 // Optionally listen for and handle events
 // (See the Emitted events section for more)
-// userManager.on(EVENT_NAME, function(data) {
-  // do something with data
-// });
+userManager.on("loginSuccess", function(data) {
+  console.log(data)
+});
